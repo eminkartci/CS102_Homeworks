1248a39e8cd8dc774b2089dc66190961317416a9
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-17 10:09:06 +0300

    getter for Profit & Revenue / tostring Method /  Print financial Statements

--- src/src/AppleStore.java
+++ src/src/AppleStore.java
@@ -28,9 +28,35 @@ public class AppleStore{
         System.out.println("The exchange rate: 1 USD = "+ newRate + " TL");
     }
 
+    // calculate and return gross  revenue
     public float getRevenue(){
+        // Revenue = Unit Price * Sales Volume
         return this.macPrice * this.soldMacCount;
     }
+
+    // calculate and return profit of the store
+    public float getProfit(){
+        // Profit = Revenue - Unit Cost * Sales Volume
+        return this.getRevenue() - this.macCost * this.soldMacCount;
+    }
     
+    public String toString(){
+
+        String AppleStoreContent = "     ---------------> Apple Store <---------------\n"+
+                                   " | Apple Store Name         : "+ this.storeName     +"\n"+
+                                   " | The Cost of a MacBook    : "+ this.macCost       +"\n"+
+                                   " | The Price of a MacBook   : "+ this.macPrice      +"\n"+
+                                   " | The Excahnge Rate        : 1 USD = "+this.exchangeRate+" TL\n";
+
+
+        return AppleStoreContent;
+    }
+
+    // Print financial statements
+    public void printStoreFinancials(){
+        System.out.println("Total Number of Macs Sold: "    +this.soldMacCount );
+        System.out.println("Total Revenue: "                +this.getRevenue() );
+        System.out.println("Total Profit: "                 +this.getProfit() );
+    }
 
 }
