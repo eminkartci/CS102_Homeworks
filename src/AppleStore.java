99fafd4f89e6602f1fd5295c19026ddccdef42cb
eminkartci <emin.kartci@ozu.edu.tr>
2021-03-17 22:22:05 +0300

    save as txt finished - comments are added

--- src/AppleStore.java
+++ src/AppleStore.java
@@ -1,3 +1,5 @@
+
+// To save the store as txt file import them
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -11,13 +13,19 @@ public class AppleStore{
     private int soldMacCount;
     private int macStock;       // Amount of mac that store can sell
 
+    // Main Consturcor
+        // Get:
+            // Store Name
+            // Mac Cost
+            // Mac Price
+            // Stock
     public AppleStore(String storeName,float macCost,float macPrice,int macStock){
         this.storeName      = storeName;
-        this.exchangeRate   = (float) 7.5;
+        this.exchangeRate   = (float) 7.5; // Default 7.5
         this.macCost        = macCost;
         this.macPrice       = macPrice;
-        this.soldMacCount   = 0;
-        this.macStock       = macStock;
+        this.soldMacCount   = 0;            // Default 0
+        this.macStock       = macStock;     // Default 100
     }
 
     // Another constructor
@@ -27,26 +35,33 @@ public class AppleStore{
         this(storeName, macCost, macPrice, 100); 
     }
 
+    // Sell Macs
     public void sellMacs(int numberOfMacsSold){
-
+        // Check if stock is enough or the number is valid
         if (this.checkMacsSold(numberOfMacsSold)){
+            // increase the sold count
             this.soldMacCount += numberOfMacsSold;
+            // inform the user
             System.out.println(numberOfMacsSold + " macs sold.");
+        // Otherwise
         }else {
+            // inform the user
             System.out.println("The stock is limited: " + this.macStock);
             System.out.println("The number of macs sold " + numberOfMacsSold + " is not valid! Try again !!");
         }
     }
 
     public void setExchangeRate(float newRate){
-
+        // check if the rate is valid
         if (this.checkExchangeRate(newRate)){
-
+            // set the non-primitive variable
             this.exchangeRate = newRate;
-
+            // inform
             System.out.println("\nThe exchange rate is changed !!");
             System.out.println("The exchange rate: 1 USD = "+ newRate + " TL");
+        // OW
         }else {
+            // inform 
             System.out.println("The given exchange rate: " + newRate + " is not valid! Try again !!");
         }
         
@@ -64,23 +79,35 @@ public class AppleStore{
         return this.getRevenue() - (this.macCost * this.soldMacCount) * this.exchangeRate;
     }
     
+    
     public String toString(){
-
+        // initialize a string 
         String AppleStoreContent = "\n     ---------------> Apple Store <---------------\n"+
                                    " | Apple Store Name         : "+ this.storeName     +"\n"+
                                    " | The Cost of a MacBook    : "+ this.macCost       +"\n"+
                                    " | The Price of a MacBook   : "+ this.macPrice      +"\n"+
                                    " | The Excahnge Rate        : 1 USD = "+this.exchangeRate+" TL\n";
 
-
+        // return it
         return AppleStoreContent;
     }
 
     // Print financial statements
     public void printStoreFinancials(){
+        // Print to the screen
         System.out.println("\nTotal Number of Macs Sold: "    +this.soldMacCount );
         System.out.println("Total Revenue: "                +this.getRevenue() );
         System.out.println("Total Profit: "                 +this.getProfit() );
+    }
+
+    // return financial statements
+    public String toStringStoreFinancials(){
+        // initialize a string
+        String financialContent =   "\n\nTotal Number of Macs Sold: "    +this.soldMacCount
+                                    +"\nTotal Revenue: "                +this.getRevenue()
+                                    +"\nTotal Profit: "                 +this.getProfit();
+        // return it
+        return financialContent;
     }
 
 
@@ -108,14 +135,29 @@ public class AppleStore{
         return false;
     }
 
+    // save store as txt file
     public void saveStoreAsTxt(){
-
+        // use try catch
         try {
+            // create a filewriter instance
+                                                // delete spaces in name instead use _
+                                                // add .txt extension
             FileWriter myWriter = new FileWriter(new File(this.storeName.replace(' ', '_')+".txt"));
+
+            // Write to the file
             myWriter.write(this.toString());
+            myWriter.write(this.toStringStoreFinancials());
+            myWriter.write("\n\nAll Rights Reserved @2021 Emin Kartci");
+            // Close the file properly
             myWriter.close();
+
+            // inform
             System.out.println("The store is successfully saved.");
+
+        // If any problem occurs
           } catch (IOException e) {
+
+            // inform
             System.out.println("A problem occured while saving the store!!");
             e.printStackTrace();
           }
--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,29 @@ public class Main {
 
     public static void main(String[] args){
 
+
+        // create an AppleStore instance 
+            // Store Name -> Zorlu Apple store
+            // Mac Cost   -> 1200
+            // Mac Price  -> 1500
         AppleStore store1 = new AppleStore("Zorlu Apple Store",1200,1500);
 
+        // Print the infomration
         System.out.println(store1.toString());
 
+        // Sell 10 macs
         store1.sellMacs(10);
 
+        // Sell 5 macs more
         store1.sellMacs(5);
 
+        // Check the financial situation
         store1.printStoreFinancials();
 
+        // Change the exchange rate
         store1.setExchangeRate(8);
 
+        // Save the store values
         store1.saveStoreAsTxt();
 
     }
