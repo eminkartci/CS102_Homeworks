708e5397b9e30ef053b756b3e17e5de4bcdeb47e
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-14 18:07:20 +0300

    Point print method - Point array

--- src/Point.java
+++ src/Point.java
@@ -9,5 +9,10 @@ public class Point {
         this.x = x;
         this.y = y;
     }
+
+    public void print_point(){
+        String pointInfo = "x coordinate: "+ this.x + " & y coordinate: " + this.y;
+        System.out.println(pointInfo);
+    }
     
 }
--- src/Quadrilateral.java
+++ src/Quadrilateral.java
@@ -2,17 +2,15 @@
 
 public class Quadrilateral {
 
-    private Point point1;
-    private Point point2;
-    private Point point3;
-    private Point point4;
+    private Point[] points = new Point[4];
+
 
     // Main constructure 
     public Quadrilateral(Point p1,Point p2,Point p3,Point p4){
-        this.point1 = p1;
-        this.point1 = p2;
-        this.point1 = p3;
-        this.point1 = p4;
+        this.points[0] = p1;
+        this.points[1] = p2;
+        this.points[2] = p3;
+        this.points[3] = p4;
     }
     
 }
