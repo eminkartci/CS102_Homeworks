3302889d72a9ede422a9a1247c46ba63e80142d4
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-14 18:17:37 +0300

    implement all print methods by overwriting

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -3,5 +3,19 @@ public class Rectangle extends Parallelogram{
     public Rectangle(Point p1, Point p2, Point p3, Point p4) {
         super(p1, p2, p3, p4);
     }
+
+    // to print console 
+    public void print_console(){
+        // type 
+        System.out.println(" --- Parallelogram --- ");
+
+        // Points
+        for( int i = 0 ; i < 4 ; i++){
+            // order of the points starting from 1 to 4
+            System.out.print( "Point " + (i + 1)+": ");
+            // point coordinate
+            getPoints()[i].print_point();
+        }
+    }
     
 }
--- src/Square.java
+++ src/Square.java
@@ -5,5 +5,19 @@ public class Square extends Rectangle{
     public Square(Point p1, Point p2, Point p3, Point p4) {
         super(p1, p2, p3, p4);
     }
+
+    // to print console 
+    public void print_console(){
+        // type 
+        System.out.println(" --- Square --- ");
+
+        // Points
+        for( int i = 0 ; i < 4 ; i++){
+            // order of the points starting from 1 to 4
+            System.out.print( "Point " + (i + 1)+": ");
+            // point coordinate
+            getPoints()[i].print_point();
+        }
+    }
     
 }
