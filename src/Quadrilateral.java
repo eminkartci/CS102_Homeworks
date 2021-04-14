b481867ecd24947ed734e5255f6ce8f2d887ac93
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-14 16:31:04 +0300

    Quadrilateral Class is implemented with a constructor

---
+++ src/Quadrilateral.java
@@ -0,0 +1,18 @@
+
+
+public class Quadrilateral {
+
+    private Point point1;
+    private Point point2;
+    private Point point3;
+    private Point point4;
+
+    // Main constructure 
+    public Quadrilateral(Point p1,Point p2,Point p3,Point p4){
+        this.point1 = p1;
+        this.point1 = p2;
+        this.point1 = p3;
+        this.point1 = p4;
+    }
+    
+}
