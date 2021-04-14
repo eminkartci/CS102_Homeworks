8594b3c6784dc5763b7710ae7aacd724a9b480fa
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-14 18:08:43 +0300

    print method for Quadrilateralc

--- src/Quadrilateral.java
+++ src/Quadrilateral.java
@@ -12,5 +12,19 @@ public class Quadrilateral {
         this.points[2] = p3;
         this.points[3] = p4;
     }
+
+    // to print console 
+    public void print_console(){
+        // type 
+        System.out.println(" --- Quadrilateral --- ");
+
+        // Points
+        for( int i = 0 ; i < 4 ; i++){
+            // order of the points starting from 1 to 4
+            System.out.println( "Point " + (i + 1)+": ");
+            // point coordinate
+            this.points[i].print_point();
+        }
+    }
     
 }
