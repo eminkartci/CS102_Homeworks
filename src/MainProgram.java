7f0dee1884e03c269c6847b9e38cd52a4a2357f7
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-14 18:14:29 +0300

    Main program - First Object

---
+++ src/MainProgram.java
@@ -0,0 +1,13 @@
+public class MainProgram {
+
+    public static void main(String[] args){
+
+        // Generate Objects
+
+            // Trapezoid
+            Trapezoid trapezoid = new Trapezoid(new Point(0,0),new Point(1,0),new Point(1,0),new Point(1,1));
+            trapezoid.print_console();
+
+    }
+    
+}
