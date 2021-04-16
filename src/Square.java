4d420e0574659e34fe437355ba240d983693c356
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-16 22:47:07 +0300

    Edge is added - calculateEdge Method is implemented

--- src/Square.java
+++ src/Square.java
@@ -2,8 +2,25 @@
 
 public class Square extends Rectangle{
 
+    private double edge;
+
     public Square(Point p1, Point p2, Point p3, Point p4) {
         super(p1, p2, p3, p4);
+        calculateEdge();
+    }
+
+    public void calculateEdge(){
+
+        // Find the points has same x axis
+            // Then difference of Y coordinates gives us the length of an edge
+        if ((getPoints()[0].getX()-getPoints()[1].getX()) == 0){
+            this.edge = getPoints()[0].getY() - getPoints()[1].getY();
+        }else if(getPoints()[0].getX()-getPoints()[1].getX() == 0){
+            this.edge = getPoints()[0].getY() - getPoints()[2].getY();
+        }else if(getPoints()[0].getX()-getPoints()[3].getX() == 0){
+            this.edge = getPoints()[0].getY() - getPoints()[3].getY();
+        }
+        this.edge = Math.abs(this.edge);
     }
 
     public String toString(){
