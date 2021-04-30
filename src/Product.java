f4c8beae0d1025c811ba0c8b54a715d1e3e6155d
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-30 09:13:36 +0300

    Getter setters and toStrong are implemented

--- src/Product.java
+++ src/Product.java
@@ -13,9 +13,29 @@ public class Product {
     public Product(double taxRate) {
         this(taxRate, 1.);
     }
-    
+
     public Product() {
         this(18., 1.);
+    }
+
+    public String toString() {
+        return "Name of the product is " + this.name;
+    }
+
+    public void setTaxRate(double taxRate) {
+        this.taxRate = taxRate;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
     }
     
 }
