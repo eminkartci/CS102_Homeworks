9a988f9bda0c0e2df4972f5f9b1cf064663a02fa
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-30 09:12:37 +0300

    Product class is implemented

---
+++ src/Product.java
@@ -0,0 +1,12 @@
+public class Product {
+
+    // Attributes
+    private double taxRate;
+    private double price;
+    private String name;
+
+    public Product() {
+        this(18., 1.);
+    }
+    
+}
