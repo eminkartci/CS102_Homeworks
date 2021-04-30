4c8c3371631818214ec1a777713e67f36d8e132c
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-30 09:15:40 +0300

    Trousers class is implemented

---
+++ src/Trousers.java
@@ -0,0 +1,11 @@
+
+public class Trousers extends Cloth {
+    public Trousers(double taxRate, String color, String brand) {
+        super(taxRate, color, brand);
+    }
+
+    public String toString() {
+        return super.toString() + super.getName();
+    }
+}
+
