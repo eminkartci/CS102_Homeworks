a92d72a06156a680aa8e7247fe7687801229928f
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-30 09:05:27 +0300

    prepare fruits is implemented

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -1,8 +1,31 @@
+import java.util.ArrayList;
+
+
 public class MainProgram {
 
     public static void main(String[] args){
 
 
     }
+
+    public static void prepareFruits (ArrayList<Fruit> fruits) {
+        
+        for (Fruit f : fruits){
+
+            if (f instanceof TreeFruit){
+                
+                TreeFruit tf = (TreeFruit) f;
+                tf.peel();
+
+            }else if (f instanceof GroundFruit){
+
+                GroundFruit tf = (GroundFruit) f;
+                tf.pick();
+
+            }
+
+        }
+
+   }
     
 }
