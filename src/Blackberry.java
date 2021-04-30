ac07079131d7535ca01622668d7dac0a2c59a908
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-30 08:50:52 +0300

    Blackberry class is implemented

---
+++ src/Blackberry.java
@@ -0,0 +1,12 @@
+public class Blackberry extends Fruit{
+
+    public Blackberry(){
+        setColor("Black");
+    }
+
+    @Override
+    public String getVitamin() {
+        return "C "+ "K";
+    }
+    
+}
