605bbccdf8260b2c8446cea48c366dbdb2eee8af
eminkartci <emin.kartci@ozu.edu.tr>
2021-04-30 08:49:40 +0300

    Stawberry class is implemented

---
+++ src/Strawberry.java
@@ -0,0 +1,12 @@
+public class Strawberry extends Fruit{
+
+    public Strawberry(){
+        setColor("Red");
+    }
+
+    @Override
+    public String getVitamin() {
+        return "B5 " + "E";
+    }
+    
+}
